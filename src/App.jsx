90e36d9bdba4cdd4ef0c8f59a28753fc4aeefa2d
Akshay Bhokare <akshaykishor123@gmail.com>
2026-03-18 11:59:39 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg-[var(--bg)] text-white min-h-screen">
+    <div className="bg-(--bg) text-white min-h-screen">
       <Navbar />
       <main className="pt-20 px-6">
         <Hero />
